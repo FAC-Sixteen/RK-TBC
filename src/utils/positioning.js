967c9fa277997e33995dc2b13639e71a1c53296b
Rohan Shah Summers <rohan_shah_summers@yahoo.co.uk>
2019-05-30 15:48:34 +0100

    small fix for positioning

--- src/utils/positioning.js
+++ src/utils/positioning.js
@@ -1,19 +1,6 @@
 const positioning = () => {
-  const top = Math.round(Math.random() * 100);
-  const left = Math.round(Math.random() * 100);
-  if (top > 50) {
-    const bottom = 100 - top;
-    return { bottom: bottom + "%", left: left + "%" };
-  }
-  if (left > 50) {
-    const right = 100 - left;
-    return { top: top + "%", right: right + "%" };
-  }
-  if (left > 50 && top > 50) {
-    const right = 100 - left;
-    const bottom = 100 - top;
-    return { bottom: bottom + "%", right: right + "%" };
-  }
+  const top = Math.round(Math.random() * 80);
+  const left = Math.round(Math.random() * 90);
   return { top: top + "%", left: left + "%" };
 };
 
